4184042638869bcccdd8b91537b535607aa2eb9b
Alexxx180 <alexx120702@gmail.com>
2022-06-12 13:25:02 +0300

    NOT WORKABLE 1.038 | Clean up content control

--- Desert-Rage/Controls/Menu/Bestiary/BestiaryLayout.xaml.cs
+++ Desert-Rage/Controls/Menu/Bestiary/BestiaryLayout.xaml.cs
@@ -1,6 +1,9 @@
-﻿using DesertRage.ViewModel;
+﻿using DesertRage.Model.Locations.Battle.Stats.Enemy;
+using DesertRage.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -29,11 +32,38 @@ namespace DesertRage.Controls.Menu.Bestiary
             set => SetValue(PlayerProperty, value);
         }
 
+        public static readonly DependencyProperty
+            SelectedFoeProperty = DependencyProperty.Register(
+                nameof(SelectedFoe), typeof(Foe), typeof(BestiaryLayout));
+
+        public Foe SelectedFoe
+        {
+            get => GetValue(SelectedFoeProperty) as Foe;
+            set => SetValue(SelectedFoeProperty, value);
+        }
+        
         public BestiaryLayout()
         {
             InitializeComponent();
         }
 
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        /// <summary>
+        /// Raises this object's PropertyChanged event.
+        /// </summary>
+        /// <param name="propertyName">The property that has a new value.</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+
+        #endregion
     }
 }
