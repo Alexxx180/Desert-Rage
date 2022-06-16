b9799100b9a5bb8346698e94ff0026bc981fa279
Alexxx180 <alexx120702@gmail.com>
2022-06-17 00:28:16 +0300

    Setting converters

---
+++ UI/Bindings/Converters/DoubleConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using DesertRage.Model.Helpers;
+
+namespace DesertRage.Decorators.UI.Bindings.Converters
+{
+    public class DoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToDouble() / 100.00;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToDouble() * 100.00;
+        }
+    }
+}
---
+++ UI/Bindings/Converters/InvertConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using DesertRage.Model.Helpers;
+
+namespace DesertRage.Decorators.UI.Bindings.Converters
+{
+    public class InvertConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (parameter.ToInt() - value.ToInt()) / 100.00;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return parameter.ToInt() + value.ToDouble() * 100;
+        }
+    }
+}
