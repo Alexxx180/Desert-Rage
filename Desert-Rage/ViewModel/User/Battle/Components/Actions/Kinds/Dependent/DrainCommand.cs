96178f9f4d0c5870593d06eb80294d962c92f6f6
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-07-05 14:20:40 +0300

    Update Drain Command description

--- Desert-Rage/ViewModel/User/Battle/Components/Actions/Kinds/Dependent/DrainCommand.cs
+++ Desert-Rage/ViewModel/User/Battle/Components/Actions/Kinds/Dependent/DrainCommand.cs
@@ -11,6 +11,12 @@ namespace DesertRage.ViewModel.User.Battle.Components.Actions.Kinds.Dependent
             UnitCursor = Targeting.ONE;
         }
 
+        /// <summary>
+        /// Hit selected enemy and
+        /// give its lost HPs to hero
+        /// </summary>
+        /// <param name="dependency">Damage power formula</param>
+        /// <param name="thing">Skill info</param>
         public DrainCommand(IFormula dependency,
             NoiseUnit thing) : base(dependency, thing)
         {
