96c2e6ce2272360e8dd1323a4b3fe5e20ff8377f
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-07-05 17:19:40 +0300

    Update Reinforcement description

--- Desert-Rage/ViewModel/User/Battle/Components/Participation/Statuses/Reinforcement.cs
+++ Desert-Rage/ViewModel/User/Battle/Components/Participation/Statuses/Reinforcement.cs
@@ -4,6 +4,10 @@ namespace DesertRage.ViewModel.User.Battle.Components.Participation.Statuses
 {
     public class Reinforcement : StatusEvent
     {
+        /// <summary>
+        /// Simple "booster" base for status event
+        /// <param name="participant">Battle participant</param>
+        /// <param name="status">Required status</param>
         public Reinforcement(Participant participant,
             StatusID status) : base(participant)
         {
