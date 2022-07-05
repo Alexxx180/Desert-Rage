535b328436fb16ff646e0ecf64177d604ee207d6
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-07-05 16:33:37 +0300

    Update IEnemyAppearing description

--- Desert-Rage/ViewModel/User/Battle/Components/Strategy/Appear/IEnemyAppearing.cs
+++ Desert-Rage/ViewModel/User/Battle/Components/Strategy/Appear/IEnemyAppearing.cs
@@ -7,6 +7,11 @@ namespace DesertRage.ViewModel.User.Battle.Components.Strategy.Appear
 {
     public interface IEnemyAppearing
     {
+        /// <summary>
+        /// Determines how enemies should 
+        /// be placed on the battle field
+        /// </summary>
+        
         public List<Tuple<Position, Foe>> Build();
         public void ResetEnemies(params Foe[] foes);
     }
