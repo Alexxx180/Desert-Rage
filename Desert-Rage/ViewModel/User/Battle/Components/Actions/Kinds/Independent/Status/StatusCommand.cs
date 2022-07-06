da9e98c941ba46861dd60f9dbbc1dcaea40ad10a
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-07-06 14:43:06 +0300

    Update Status Command: attributes

--- Desert-Rage/ViewModel/User/Battle/Components/Actions/Kinds/Independent/Status/StatusCommand.cs
+++ Desert-Rage/ViewModel/User/Battle/Components/Actions/Kinds/Independent/Status/StatusCommand.cs
@@ -5,21 +5,19 @@ using System.ComponentModel;
 namespace DesertRage.ViewModel.User.Battle.Components.Actions.Kinds.Independent.Status
 {
     public class StatusCommand : ActCommand, IAction, INotifyPropertyChanged
-    {
-        public StatusCommand(NoiseUnit thing) : base(thing)
+    {        
+        /// <summary>
+        /// Status container
+        /// </summary>
+        public StatusCommand()
         {
             UnitCursor = Targeting.HERO;
         }
 
-        /// <summary>
-        /// Status container
-        /// </summary>
-        /// <param name="status">Required status</param>
-        /// <param name="thing">Thing info</param>
-        public StatusCommand
-            (StatusID status, NoiseUnit thing) : this(thing)
+        public void SetUnit(AttributeUnit unit)
         {
-            Status = status;
+            base.SetUnit(unit);
+            Status = unit.Attributes["Status"];
         }
 
         public virtual void Use(object parameter)
