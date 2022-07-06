3fa96839cf185b1bd9a2318f9db4198888bf9acf
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-07-06 12:18:34 +0300

    Create Command ID storage

---
+++ Locations/Battle/Things/Storage/CommandID.cs
@@ -0,0 +1,27 @@
+namespace DesertRage.Model.Locations.Battle.Things.Storage
+{
+    public enum CommandID
+    {
+        Cure,
+        Rest,
+        Recover,
+        Drain,
+        DrainAll,
+        Osmose,
+        OsmoseAll,
+        Fight,
+        FightAll,
+        Weapon,
+        Escape,
+        
+        CureMax,
+        RestMax,
+        RecoverMax,
+        Annihilate,
+        AnnihilateAll,
+        Learn,
+        
+        ApplyStatus,
+        RemoveStatus
+    }
+}
