9c05eed7e5ed42c1a0a52b3ad7dc746740bf4cbc
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-07-06 16:30:40 +0300

    Instant Command attribute unit update

--- Desert-Rage/ViewModel/User/Battle/Components/Actions/InstantCommand.cs
+++ Desert-Rage/ViewModel/User/Battle/Components/Actions/InstantCommand.cs
@@ -7,8 +7,26 @@ namespace DesertRage.ViewModel.User.Battle.Components.Actions
         /// <summary>
         /// Can be used basing on effect, e.g. forever
         /// </summary>
-        /// <param name="action">What need to be done</param>
-        public InstantCommand(IAction action) : base(action) { }
+        
+        private static readonly string _commandSpace;
+    
+        static InstantCommand()
+        {
+            Type command = typeof(DesertRage.ViewModel.User.Battle.Components.Actions.Kinds.ActCommand);
+            _commandSpace = command.NameSpace;
+        }
+        
+        private protected virtual SetUnit(string commandName, AttributeUnit unit)
+        {
+            IAction action = (IAction)Activator.CreateInstance(commandName);
+            action.SetUnit(unit);
+            Effect = action;
+        }
+
+        public void SetUnit(AttributeUnit unit)
+        {
+            SetUnit($"{_commandSpace}.{unit.Command}", unit);
+        }
 
         public override void Execute(object parameter)
         {
