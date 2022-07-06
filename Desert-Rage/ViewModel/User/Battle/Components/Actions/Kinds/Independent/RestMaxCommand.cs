37d14fd24b9bbcfbc741c25dce91093245932165
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-07-06 15:21:09 +0300

    Rest Max Command reset to default constructor

--- Desert-Rage/ViewModel/User/Battle/Components/Actions/Kinds/Independent/RestMaxCommand.cs
+++ Desert-Rage/ViewModel/User/Battle/Components/Actions/Kinds/Independent/RestMaxCommand.cs
@@ -7,11 +7,7 @@ namespace DesertRage.ViewModel.User.Battle.Components.Actions.Kinds.Independent
         /// <summary>
         /// Fully refills hero AP
         /// </summary>
-        /// <param name="thing">Thing info</param>
-        public RestMaxCommand(NoiseUnit thing) : base(thing)
-        {
-            UnitCursor = Targeting.HERO;
-        }
+        public RestMaxCommand() : base() { }
 
         public override void Use(object parameter)
         {
