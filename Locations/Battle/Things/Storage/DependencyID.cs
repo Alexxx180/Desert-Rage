6461d43d91a077a05b01e11b5ce3f990f62cfd86
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-07-06 17:40:58 +0300

    Dependency ID storage enum

---
+++ Locations/Battle/Things/Storage/DependencyID.cs
@@ -0,0 +1,11 @@
+namespace DesertRage.Model.Locations.Battle.Things.Storage
+{
+    public enum DependencyID : int
+    {
+        ItemFormula,
+        SpeedFormula,
+        AttackFormula,
+        SpecialFormula,
+        MergedFormula
+    }
+}
