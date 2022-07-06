29633cf686ce259383dce5f5300591f1a002c56f
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-07-06 14:53:06 +0300

    Update Apply Status - reset to default constructor

--- Desert-Rage/ViewModel/User/Battle/Components/Actions/Kinds/Independent/Status/ApplyStatus.cs
+++ Desert-Rage/ViewModel/User/Battle/Components/Actions/Kinds/Independent/Status/ApplyStatus.cs
@@ -9,10 +9,6 @@ namespace DesertRage.ViewModel.User.Battle.Components.Actions.Kinds.Independent.
         /// <summary>
         /// Apply specified status
         /// </summary>
-        /// <param name="status">Required status</param>
-        /// <param name="thing">Thing info</param>
-        public ApplyStatus
-            (StatusID status, NoiseUnit thing) : base(status, thing) { }
 
         public override void Use(object parameter)
         {
