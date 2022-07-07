708af3dcab9e818f3588fd85294b740ac2c49cc5
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-07-07 16:28:28 +0300

    Location Floor Area

--- Locations/Location.cs
+++ Locations/Location.cs
@@ -1,59 +1,19 @@
 ﻿using DesertRage.Model.Locations.Battle.Stats.Enemy.Storage;
 using DesertRage.Model.Locations.Battle.Stats.Player.Armory;
+using DesertRage.Model.Locations.Map;
 using System.Collections.Generic;
 
 namespace DesertRage.Model.Locations
 {
-    public class Location
+    public class Location : Unit
     {
-        public Location()
+        public void Set(Location next)
         {
-            Danger = new Chip();
-        }
-
-        public void SetChapter(Location next)
-        {
-            Name = next.Name;
-            NextChapter = next.NextChapter;
-            Map = next.Map;
-            BackCover = next.BackCover;
-            BattleBack = next.BattleBack;
-            MusicPeace = next.MusicPeace;
-            MusicFight = next.MusicFight;
-            Start = next.Start;
-            Danger.Set(next.Danger);
-            StageFoes = next.StageFoes;
-            Bosses = next.Bosses;
+            base.Set(next);
             Messages = next.Messages;
-            Gates = next.Gates;
-            Warps = next.Warps;
-            Equipment = next.Equipment;
-            TileCodes = next.TileCodes;
-            IsTimeChamber = next.IsTimeChamber;
         }
 
-        public string Name { get; set; }
-        public string NextChapter { get; set; }
-
-        public char[][] Map { get; set; }
-        public string BackCover { get; set; }
-        public string BattleBack { get; set; }
-
-        public string MusicPeace { get; set; }
-        public string MusicFight { get; set; }
-
-        public Position Start { get; set; }
-        public Chip Danger { get; set; }
-        public bool IsTimeChamber { get; set; }
-
-        public EnemyBestiary[] StageFoes { get; set; }
-        public Dictionary<string, EnemyBestiary> Bosses { get; set; }
-
+        public Floor Area { get; set; }
         public Dictionary<string, string> Messages { get; set; }
-        public Dictionary<string, Position> Gates { get; set; }
-        public Dictionary<string, Position> Warps { get; set; }
-        public Dictionary<string, ArmoryElement> Equipment { get; set; }
-
-        public Dictionary<string, string> TileCodes { get; set; }
     }
 }
